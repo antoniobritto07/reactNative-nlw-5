78ce0dd5decf94b3aa4862bb67dd2877f61cd688
Antonio Britto <antonio.britto.2000@hotmail.com>
2021-04-26 23:27:43 -0300

    Feat: Incluindo as fontes  ao nosso app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Welcome } from './src/pages/Welcome';
+import AppLoading from 'expo-app-loading';
 //usando fonts diretamente com uma funcionalidade do expo chamada @expo google fonts
+
+import Routes from './src/routes';
+
 import {
   useFonts,
-  Jost_400Regular,
-  Jost_600SemiBold
+  Jost_600SemiBold,
+  Jost_400Regular
 } from '@expo-google-fonts/jost';
 
-import AppLoading from 'expo-app-loading';
 // expo install expo-app-loading -> instalação para conseguir segurar a tela de splash enquanto a tela estiver carregando( basicamente enquanto os elementos nao tenham ainda carregados )
 // fica aparecendo a imagem enquanto as coisas, como a fonte, nao estiver carregada ainda
 
@@ -16,12 +17,12 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
-  })
+  });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    <AppLoading />
   }
   return (
-    <Welcome />
+    <Routes />
   )
 }
