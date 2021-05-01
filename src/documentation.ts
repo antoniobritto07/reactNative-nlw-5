a271bf983c3b404bbef421862882634341305261
Antonio Britto <antonio.britto.2000@hotmail.com>
2021-05-01 18:18:17 -0300

    Feat: Começando a fazer documentação do projeto para facillitar posteriormente

---
+++ src/documentation.ts
@@ -0,0 +1,5 @@
+// Async Storage é uma ferramenta usada para armazenar dados no proprio dispositivo
+// movel da pessoa que está usando a aplicação, da mesma forma que trabalha o
+// localStorage. Basca buscar no google por Expo Async Storage e seguir os passos
+
+// colocamos o @plantmanager:user só para ficar de forma mais semântica para a aplicação
