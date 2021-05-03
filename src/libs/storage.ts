7d7bc3ab54a744402d9b5e54f2ed25a76edaf17c
Antonio Britto <antonio.britto.2000@hotmail.com>
2021-05-03 03:20:27 -0300

    Feat: Fazendo toda a lógica para o user ser notificado no seu dispositivo quando precisar

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Notifications from 'expo-notifications';
 import { format } from 'date-fns';
 
 export interface PlantProps {
@@ -11,26 +12,59 @@ export interface PlantProps {
   frequency: {
     times: number;
     repeat_every: string;
-  },
+  };
   hour: string,
   dateTimeNotification: Date;
 }
 
-interface StoragePlantProps {
+export interface StoragePlantProps {
   [id: string]: {
     data: PlantProps;
+    notificationID: string;
   }
 }
 
 //algoritmo que salva as plantas que a pessoa selecionar no localStorage do celular dela
 export async function savePlant(plant: PlantProps): Promise<void> {
   try {
+    //lógica de notificação para o usuário
+    const nextTime = new Date(plant.dateTimeNotification);
+    const now = new Date();
+
+    const { times, repeat_every } = plant.frequency;
+    if (repeat_every === 'week') {
+      const interval = Math.trunc(7 / times);
+      nextTime.setDate(now.getTime() + interval);
+    } else {
+      nextTime.setDate(nextTime.getDate() + 1)
+    }
+    const seconds = Math.abs(
+      Math.ceil((now.getTime() - nextTime.getTime()) / 1000)
+    )
+
+    const notificationID = await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Heeey, 🌵', //peguei emoji do site getemoji.com
+        body: `Está na hora de cuidar da sua ${plant.name}`, //corpo da notificação
+        sound: true, //se vai tocar algum som quando notificar
+        priority: Notifications.AndroidNotificationPriority.HIGH, //prioridade em cada dispositivo
+        data: {
+          plant //data que pode ir junto com as notificações
+        },
+      },
+      trigger: { //aqui é onde vem a ideia da repetição da alerta
+        seconds: seconds < 60 ? 60 : seconds, //tem que agendar pelo menos 60 segundos, por padrão
+        repeats: true
+      }
+    })
+
     const data = await AsyncStorage.getItem('@plantmanager:plants');
     const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
     const newPlant = {
       [plant.id]: {
-        data: plant
+        data: plant,
+        notificationID
       }
     }
 
@@ -68,4 +102,18 @@ export async function loadPlant() {
   } catch (error) {
     throw new Error(error);
   }
+}
+
+//algoritmo para fazer a remoção de uma planta do async storage do usuário
+export async function removePlant(id: string): Promise<void> {
+  const data = await AsyncStorage.getItem('@plantmanager:plants');
+  const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+  await Notifications.cancelScheduledNotificationAsync(plants[id].notificationID)
+  delete plants[id];
+
+  await AsyncStorage.setItem(
+    '@plantmanager:plants',
+    JSON.stringify(plants)
+  );
 }
